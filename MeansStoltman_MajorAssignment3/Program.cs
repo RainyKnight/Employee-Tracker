5fccff3933f6d14990eb70d48f90c2069ad5e50e
Lucas Stoltman Mac <stoltmanlucas@gmail.com>
2019-11-18 06:28:37 -0800

    Initialized the project

---
+++ MeansStoltman_MajorAssignment3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MeansStoltman_MajorAssignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
