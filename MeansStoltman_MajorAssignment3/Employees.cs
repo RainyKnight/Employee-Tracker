52709e735a46b442cd87bf3f1272d0a3a1df3d14
Andrew Means <andrewmmeans@gmail.com>
2019-11-21 17:06:00 -0800

    Added View, Hierarchy, Hire, Manager method logic

--- MeansStoltman_MajorAssignment3/Employees.cs
+++ MeansStoltman_MajorAssignment3/Employees.cs
@@ -4,6 +4,7 @@
 // Purpose: Create an application to be used by a company to keep track of 
 // and organize employees by their salaries, hierarchy, and order of promotion.
 using System;
+using System.Collections.Generic;
 
 namespace MeansStoltman_MajorAssignment3
 {
@@ -11,9 +12,30 @@ namespace MeansStoltman_MajorAssignment3
     {
         static void Main(string[] args)
         {
+            // Setup
             bool moreUserIteractions = true;
 
-            // TODO populate the data structures to traverse
+            // Create initial employees
+            Employee adam = new Employee("Adam", 110000, null, null);
+            Employee sarah = new Employee("Sarah", 115000, null, null);
+            Employee raj = new Employee("Raj", 90000, sarah, null);
+            Employee marcus = new Employee("Marcus", 80000, sarah, raj);
+            Employee anna = new Employee("Anna", 95000, adam, null);
+            Employee lucas = new Employee("Lucas", 100000, adam, anna);
+
+            adam.SetNext(lucas);
+            sarah.SetNext(marcus);
+
+            // Initialize a promotion queue
+            Queue promotionQueue = new Queue();
+
+            List<Employee> emps = new List<Employee>{ sarah, adam, lucas, anna, raj, marcus };
+
+            // Add employees to queue in order to be promoted
+            foreach (Employee emp in emps)
+            {
+                promotionQueue.Enqueue(raj);
+            }       
 
             // Greet the user
             Intro();
@@ -22,73 +44,187 @@ namespace MeansStoltman_MajorAssignment3
             while (moreUserIteractions)
             {
                 // Prompt user for input
-                Input();
+                string userInput = Input();
+
+                if (userInput == "FIND")
+                {
+                    // TODO: Binary Search for salary
+                }
+                else if (userInput == "VIEW")
+                {
+                    Console.WriteLine("Here is the list of employees:");
+                    for (int i = 0; i < emps.Count; i++)
+                    {
+                        if (i == emps.Count - 1)
+                        {
+                            Console.Write(emps[i].GetName());
+                        }
+                        else
+                        {
+                            Console.Write($"{emps[i].GetName()}, ");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+                else if (userInput == "PROMOTE")
+                {
+                    // TODO: Leverage Employee.Promote()
+                }
+                else if (userInput == "HIERARCHY")
+                {
+                    Console.WriteLine("Which manager should I show the hierarchy for?");
+
+                    string managerName = Console.ReadLine();
+
+                    foreach (Employee emp in emps)
+                    {
+                        if (managerName == emp.GetName())
+                        {
+                            Employee temp = emp;
+                            List<Employee> managedEmployees = new List<Employee>();
+
+                            while (temp.GetNext() != null)
+                            {
+                                managedEmployees.Add(temp.GetNext());
+                                temp = temp.GetNext();
+                            }
+
+                            Console.Write($"The employees managed by {emp.GetName()} include ");
+                            for (int i = 0; i < managedEmployees.Count; i++)
+                            {
+                                if (i < managedEmployees.Count - 1)
+                                {
+                                    Console.Write($"{managedEmployees[i].GetName()}, ");
+                                }
+                                else
+                                {
+                                    Console.Write($"{managedEmployees[i].GetName()}.");
+                                }
+                            }
+                        }
+                    }
+
+                }
+                else if (userInput == "MANAGER")
+                {
+                    Console.WriteLine("Which employee should I show the manager of?");
+
+                    string employeeName = Console.ReadLine();
+
+                    foreach (Employee emp in emps)
+                    {
+                        if (emp.GetName() == employeeName)
+                        {
+                            Console.WriteLine($"The manager of {employeeName} " +
+                                $"is {emp.GetManager().GetName()}");
+                        }
+                    }
+                }
+                else if (userInput == "HIRE")
+                {
+                    Console.WriteLine("What is the new employee's name?");
+
+                    string empName = Console.ReadLine();
+
+                    Console.WriteLine("What is the employee's starting salary?");
+
+                    double empSalary = double.Parse(Console.ReadLine());
+
+                    Console.Write("Who is the employee's manager? Options are: ");
+                    foreach (Employee emp in emps)
+                    {
+                        if (emp.GetManager() == null)
+                        {
+                            Console.Write($"{emp.GetName()} ");
+                        }
+                    }
+                    string managerName = Console.ReadLine();
+
+                    Employee empManager = null;
+
+                    foreach (Employee emp in emps)
+                    {
+                        if (emp.GetName() == managerName)
+                        {
+                            empManager = emp;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Could not locate manager, employee has no manager");
+                        }
+                    }
+
+                    // Create new employee
+                    Employee newEmployee = new Employee(empName, empSalary, empManager, null);
+
+                    Employee temp = empManager;
+                    
+                    List<Employee> managedEmployees = new List<Employee>();
+
+                    while (temp.GetNext() != null)
+                    {
+                        managedEmployees.Add(temp.GetNext());
+                        temp = temp.GetNext();
+                    }
+
+                    // Add pointer from the last employee under the same manager
+                    // To the new employee so they are still discoverable
+                    if (managedEmployees.Count > 0)
+                    {
+                        Employee lastEmployee = managedEmployees[managedEmployees.Count - 1];
+
+                        for (int i = 0; i < emps.Count; i++)
+                        {
+                            if (emps[i].GetName() == lastEmployee.GetName())
+                            {
+                                emps[i].SetNext(newEmployee);
+                            }
+                        }
+                    }
+
+                    // Add new employee to roster, will be locatable via FIND
+                    emps.Add(newEmployee);
+
+                    // New employee will be promoted last
+                    promotionQueue.Enqueue(newEmployee);
+
+                }
+                else
+                {
+                    Console.WriteLine("Please enter \"FIND\",  \"VIEW\", \"PROMOTE\", \"HIERARCHY\", \"HIRE\", or \"MANAGER\".");
+                }
             }
 
+        }
+
+        static string Input()
+        {
+            string userInput = Console.ReadLine();
+
+            return userInput;
         }
 
         static void Intro()
         {
             Console.WriteLine("Welcome to Macrohard Employee Database" +
-                " \nWhat would you like to do?");
-        }
-
-        static void Input()
-        {
-            string userInput = Console.ReadLine();
-
-            switch (userInput)
-            {
-                // FIND
-                // Recursively traverse the binary search tree for the employee
-                // with a matching salary
-                case "FIND":
-                    //stuff
-                    break;
-
-                // VIEW
-                // Traverse the linkedlist and print out the employees
-                case "VIEW":
-                    //stuff
-                    break;
-
-                // For promotions, the employees are promoted based on merit
-                // and the order of promotion is reflected in a queue
-                // after a promotion we add the promotee to the end of the queue
-                // for promotion next cycle
-                case "PROMOTE":
-                    //stuff
-                    break;
-
-                case "HIERARCHY":
-                    //stuff
-                    break;
-
-                case "MANAGER":
-                    //stuff
-                    break;
-
-                default:
-                    Console.WriteLine("Please enter \"FIND\",  \"VIEW\", \"PROMOTE\", \"HIERARCHY\", or \"MANAGER\".");
-                    Input();
-                    break;
-            }
+                " \nWhat would you like to do?\n");
         }
     }
 
+    // Employee class acts as linkedlist of employees
     class Employee
     {
         string name;
         double salary;
         Employee manager;
-        LinkedList employees;
+        Employee next;
 
-        public Employee(string name, double salary, Employee manager, LinkedList employees)
+        public Employee(string name, double salary, Employee manager, Employee next)
         {
             this.name = name;
             this.salary = salary;
             this.manager = manager;
-            this.employees = employees;
+            this.next = next;
         }
 
         public void Promote()
@@ -106,6 +242,21 @@ namespace MeansStoltman_MajorAssignment3
         public string GetName()
         {
             return this.name;
+        }
+
+        public Employee GetNext()
+        {
+            return this.next;
+        }
+
+        public void SetNext(Employee next)
+        {
+            this.next = next;
+        }
+
+        public Employee GetManager()
+        {
+            return this.manager;
         }
     }
 
@@ -150,7 +301,8 @@ namespace MeansStoltman_MajorAssignment3
             }
             else
             {
-                root.insertData(ref root, d);
+                Console.WriteLine();
+                // root.insertData(ref root, d);
             }
 
             count++;
@@ -158,23 +310,41 @@ namespace MeansStoltman_MajorAssignment3
 
         public bool search(int s)
         {
-            return root.search(root, s);
+            return false;
+            //return root.search(root, s);
         }
 
         // TODO: Add methods to rearrange the BST if an employee is promoted
     }
 
-    // We may be able to use the .NET built-in Queue & LinkedList classes
-    class LinkedList
+    class Queue
     {
-        // TODO: add variables
-
-        public LinkedList()
+        List<Employee> employeeList;
+        int size;
+        public Queue()
         {
-            // TODO: fill in constructor
+            employeeList = new List<Employee>();
+            size = 0;
         }
 
-        // TODO: add first/last methods
-        // TODO: add addLast methods
+        public void Enqueue(Employee emp)
+        {
+            employeeList.Add(emp);
+            size++;
+
+        }
+
+        public Employee Dequeue()
+        {
+            Employee temp = employeeList[0];
+            employeeList.RemoveAt(0);
+            size--;
+            return temp;
+        }
+
+        public int GetSize()
+        {
+            return size;
+        }
     }
 }
